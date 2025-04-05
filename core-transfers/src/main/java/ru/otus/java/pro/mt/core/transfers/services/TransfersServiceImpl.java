1c3cfba6a31df3709cd6d2e294beb5a1a9a2f65e
KristinaGovorova <k.govorova94@mail.ru>
2025-04-05 11:02:42 +0300

    hw3 done

--- core-transfers/src/main/java/ru/otus/java/pro/mt/core/transfers/services/TransfersServiceImpl.java
+++ core-transfers/src/main/java/ru/otus/java/pro/mt/core/transfers/services/TransfersServiceImpl.java
@@ -1,6 +1,10 @@
 package ru.otus.java.pro.mt.core.transfers.services;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import ru.otus.java.pro.mt.core.transfers.configs.properties.TransfersProperties;
 import ru.otus.java.pro.mt.core.transfers.dtos.ExecuteTransferDtoRq;
@@ -21,6 +25,7 @@ public class TransfersServiceImpl implements TransfersService {
     private final TransfersRepository transfersRepository;
     private final TransferRequestValidator transferRequestValidator;
     private final TransfersProperties transfersProperties;
+    private final MeterRegistry meterRegistry;
 
     private final KafkaTransferProducer kafkaTransferProducer;
 
@@ -31,19 +36,32 @@ public class TransfersServiceImpl implements TransfersService {
 
     @Override
     public List<Transfer> getAllTransfers(String clientId) {
-        return transfersRepository.findAllByClientId(clientId);
+        return getAllTransfers(clientId, PageRequest.of(0, 20)).getContent();
+    }
+
+    @Override
+    public Page<Transfer> getAllTransfers(String clientId, Pageable pageable) {
+
+        int size = Math.min(pageable.getPageSize(), 1000);
+        Pageable limitedPageable = PageRequest.of(pageable.getPageNumber(), size);
+
+        return transfersRepository.findAllByClientId(clientId, limitedPageable);
     }
 
     @Override
     public void execute(String clientId, ExecuteTransferDtoRq executeTransferDtoRq) {
+        meterRegistry.counter("transfer_requests_total").increment();
         transferRequestValidator.validate(executeTransferDtoRq);
 
         if (executeTransferDtoRq.getAmount().compareTo(transfersProperties.getMaxTransferSum()) > 0) {
+            meterRegistry.counter("transfer_failure_total").increment();
             throw new BusinessLogicException("OOPS", "OOPS_CODE");
         }
 
         Transfer transfer = new Transfer(UUID.randomUUID().toString(), "1", "2", "1", "2", "Demo", BigDecimal.ONE);
         save(transfer);
+
+        meterRegistry.counter("transfer_success_total").increment();
 
         kafkaTransferProducer.sendTransferExecutedMessage(transfer.getId());
     }
